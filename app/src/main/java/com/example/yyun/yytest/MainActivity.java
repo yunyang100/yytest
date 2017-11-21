c901bc7ceaf7acd486f0db34e79208567e54b313
yunyang100 <519492013@qq.com>
2017-11-21 10:55:41 +0800

    fix error

--- app/src/main/java/com/example/yyun/yytest/MainActivity.java
+++ app/src/main/java/com/example/yyun/yytest/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.yyun.yytest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private ArrayAdapter<String> mAdapter;
+    private ArrayList<String> Options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +31,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button btnspinner=(Button)findViewById(R.id.btn_changetextview);
+        Button btnspinner=(Button)findViewById(R.id.btn_changespinner);
         final Spinner spinner=(Spinner)findViewById(R.id.spinner);
 
         btnspinner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Options = new ArrayList<String>();
+                Options.add("");
+                Options.add("1");
+                Options.add("2");
+                Options.add("3");
+                Options.add("4");
+                Options.add("5");
+                mAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, Options);
+                spinner.setAdapter(mAdapter);
             }
         });
 
