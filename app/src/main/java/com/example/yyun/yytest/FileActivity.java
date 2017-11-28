dc6af2036197ec74b8ea3d42162e0ccc8f5e037e
yunyang100 <519492013@qq.com>
2017-11-28 11:17:47 +0800

    2

--- app/src/main/java/com/example/yyun/yytest/FileActivity.java
+++ app/src/main/java/com/example/yyun/yytest/FileActivity.java
@@ -30,7 +30,7 @@ public class FileActivity extends BaseActivity {
         setContentView(R.layout.activity_file);
 
         Button btn_saaaaaaaaaaaaaaaveFile = (Button) findViewById(R.id.btn_savefile);
-        Button btn_readFile = (Button) findViewById(R.id.btn_readfile);
+        Button btn_readFileddddddddddd = (Button) findViewById(R.id.btn_readfile);
 
 
         final EditText editText = (EditText) findViewById(R.id.editfile);
